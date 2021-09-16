69683d292fd599979e744e7a22f3a31f83492bc6
rGudavadze <roma.gudavadze@gmail.com>
2021-09-16 17:21:21 +0400

    auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
-const { error } = require('console')
 const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
-const { mainModule } = require('process')
+const { promisify } = require('util')
 const User = require('./../models/userModel')
 
 
@@ -19,7 +18,6 @@ const createAndSendToken = (user, statusCode, res) => {
   if(process.env.NODE_ENV === 'production') cookieOptions.secure = true
 
   res.cookie('jwt', token, cookieOptions)
-
   // Removes the password from the output
   user.password = undefined
 
@@ -57,11 +55,11 @@ exports.signup = async(req, res) => {
 exports.login = async(req, res) => {
   try{
     const { email, password } = req.body
-    if(!email || !password) throw error("Please provide email and password")
-    const user = await User.findOne({ email }).select('+password')
+    if(!email || !password) throw new Error("Please provide email and password")
+    const user = await User.findOne({ email })
     if(!user || !(await user.correctPassword(password, user.password)))
-      throw error("Incorrecr email or pasword")
-  
+      throw new Error("Incorrecr email or pasword")
+
     createAndSendToken(user, 200, res)
     
   }catch(err){
@@ -69,5 +67,31 @@ exports.login = async(req, res) => {
       status: 'fail',
       message: err.message
     })
+  }
+}
+
+exports.protect = async(req, res, next) => {
+  try{
+    let token
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1]
+    }else if(req.cookies.jwt) {
+      token = req.cookies.jwt
+    }
+    if(!token) throw new Error('You are not logged in! Please log in to get access')
+
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+    console.log(decoded)
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id)
+    if(!currentUser) await new Error('The user belonging to this token no longer exists!')
+
+    // 4) Check if user changed password after the token was issued
+    
+
+
+  }catch(err){
+
   }
 }
--- models/userModel.js
+++ models/userModel.js
@@ -54,7 +54,7 @@ userSchema.pre(/^find/, function(next) {
   next()
 })
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', async function(next) {
   if(!this.isModified('password')) return next()
   this.password = await bcrypt.hash(this.password, 12)
   this.passwordConfirm = undefined
